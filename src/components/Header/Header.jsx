134af8a6c2a52d3227dbdbbd9eb203c84c7456be
garvit <garvitbhargava05@gmail.com>
2023-11-22 18:05:04 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,28 @@ import { getMenuStyles } from "../../utils/common";
 import useHeaderColor from "../../hooks/useHeaderColor";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link, NavLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProfileMenu from "../ProfileMenu/ProfileMenu";
+// import AddPropertyModal from "../AddPropertyModal/AddPropertyModal";
+// import useAuthCheck from "../../hooks/useAuthCheck.jsx";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   const headerColor = useHeaderColor();
+  const [modalOpened, setModalOpened] = useState(false);
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+  // const { validateLogin } = useAuthCheck();
 
+  const handleAddPropertyClick = () => {
+    if (validateLogin()) {
+      setModalOpened(true);
+    }
+  };
   return (
     <section className="h-wrapper" style={{ background: headerColor }}>
       <div className="flexCenter innerWidth paddings h-container">
         {/* logo */}
-
-        <Link to={"/"}>
+        <Link to="/">
           <img src="./logo.png" alt="logo" width={100} />
         </Link>
 
@@ -34,9 +45,17 @@ const Header = () => {
 
             <a href="mailto:zainkeepscode@gmail.com">Contact</a>
 
-            {/* login Button */}
-
-            <button className="button">Login</button>
+            {/* add property */}
+            {/* <div onClick={handleAddPropertyClick}>Add Property</div>
+            <AddPropertyModal opened={modalOpened} setOpened={setModalOpened} /> */}
+            {/* login button */}
+            {!isAuthenticated ? (
+              <button className="button" onClick={loginWithRedirect}>
+                Login
+              </button>
+            ) : (
+              <ProfileMenu user={user} logout={logout} />
+            )}
           </div>
         </OutsideClickHandler>
 
